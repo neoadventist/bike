39b1447ddfd784bb7cdf544267b5ad1721765e17
neoadventist <git@spencersims.com>
2013-08-25 01:29:03 -0700

    Map Working, coordinates set at Chief Lunes Meet Spot

--- controllers/upload.js
+++ controllers/upload.js
@@ -7,4 +7,17 @@ app.controller('upload', function ($scope, $timeout, $filter) {
 			console.log(content);
 		}
 	};
+	
+
+	// set up the map
+	map = new L.Map('map');
+	
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © OpenStreetMap contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 10, maxZoom: 20, attribution: osmAttrib});		
+
+	// start the map at Cheif Lunes Meet Spot
+	map.setView(new L.LatLng(34.033235,-118.2835057),10);
+	map.addLayer(osm);
 });
