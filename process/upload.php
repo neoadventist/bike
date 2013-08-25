e96b7b67d21257f4c8e5dd973275dcb19c90daf7
neoadventist <git@spencersims.com>
2013-08-24 23:56:04 -0700

    Able to process gpx xml

--- process/upload.php
+++ process/upload.php
@@ -1,11 +1,14 @@
 <?PHP
 
-$file = $_FILES["ride"];
 
+$file = $_FILES['ride']['tmp_name'];
+
+$contents = file_get_contents($file);
+$xml =simplexml_load_file($file);
 $u = "Upload: " . $_FILES["ride"]["name"] . "<br>";
 $t = "Type: " . $_FILES["ride"]["type"] . "<br>";
 $s = "Size: " . ($_FILES["ride"]["size"] / 1024) . " kB<br>";
 
 
-echo json_encode($u.$t.$s);
+echo json_encode($u.$t.$s.$file.print_r($xml));
 ?>
