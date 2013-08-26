fba7f17d89f0d9d868eada99072340c697eda5e8
neoadventist <git@spencersims.com>
2013-08-26 13:01:22 -0700

    upload multiple routes, changed upload map to 700px

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,38 +4,43 @@ app.controller('upload', function ($scope, $timeout, $filter) {
 	$scope.uploadComplete = function (content, completed) {
 		if (completed && content.length > 0) {
 			$scope.response = (content); // Presumed content is a json string!
-			//console.log(content);
-			drawRoute($scope.response);
+			console.log(content);
+			for($currentRoute=0;$currentRoute<$scope.response.length;$currentRoute++){
+				drawRoute($scope.response[$currentRoute]);
+			}
+			
 		}
 	};
 	
+initMap = function(){
+	// set up the map
+	window.map = new L.Map('map');
 
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © OpenStreetMap contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 20, attribution: osmAttrib});		
+
+	// start the map at Cheif Lunes Meet Spot
+	map.setView(new L.LatLng(34.033235,-118.2835057),10);
+	map.addLayer(osm);
+};
+initMap();
 	
-	drawRoute = function(route){
-		// set up the map
-		map = new L.Map('map');
-		
-		// create the tile layer with correct attribution
-		var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-		var osmAttrib='Map data © OpenStreetMap contributors';
-		var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 20, attribution: osmAttrib});		
-
-		// start the map at Cheif Lunes Meet Spot
-		map.setView(new L.LatLng(34.033235,-118.2835057),10);
-		map.addLayer(osm);
-		var data=[]; 
-		for (i=0;i<route.length;i++){
-			data.push([route[i][0], route[i][1]]);
-			console.log(route[i][0]);
-		}
-		var polyline_options = {
-			color: '#000',
-			"weight": 15,
-			"opacity": 0.65
-		};
-		var track = L.polyline(data, polyline_options).addTo(map);
-		// zoom the map to the route
-		//map.fitBounds(track.getBounds());
+drawRoute = function(route){
+	var data=[]; 
+	for (i=0;i<route.length;i++){
+		data.push([route[i][0], route[i][1]]);
+		//console.log(route[i][0]);
+	}
+	var polyline_options = {
+		color: '#000',
+		"weight": 7,
+		"opacity": 0.40
 	};
+	var track = L.polyline(data, polyline_options).addTo(window.map);
+	// zoom the map to the route
+	//window.map.fitBounds(track.getBounds());
+};
 	
 });
--- process/upload.php
+++ process/upload.php
@@ -3,21 +3,29 @@
 
 $file = $_FILES['ride']['tmp_name'];
 
-$contents = file_get_contents($file);
-$xml =simplexml_load_file($file);
-$u = "Upload: " . $_FILES["ride"]["name"] . "<br>";
-$t = "Type: " . $_FILES["ride"]["type"] . "<br>";
-$s = "Size: " . ($_FILES["ride"]["size"] / 1024) . " kB<br>";
+$n = count($file);
+$routes = Array(); 
 
-$res =$xml->trk->name;
+for($f=0;$f<$n;$f++){
+	$xml =simplexml_load_file($file[$f]);
+	/*
+	$u = "Upload: " . $_FILES["ride"]["name"] . "<br>";
+	$t = "Type: " . $_FILES["ride"]["type"] . "<br>";
+	$s = "Size: " . ($_FILES["ride"]["size"] / 1024) . " kB<br>";
+	*/
+	$res =$xml->trk->name;
 
-$route = Array(); 
+	$route = Array(); 
 
-$i=0;
-foreach($xml->trk->trkseg->trkpt as $point){
-	$route[$i][0]= (float)$point[0]["lat"];
-	$route[$i][1]= (float)$point[0]["lon"];
-	$i++;
+	$i=0;
+	foreach($xml->trk->trkseg->trkpt as $point){
+		$route[$i][0]= (float)$point[0]["lat"];
+		$route[$i][1]= (float)$point[0]["lon"];
+		$i++;
+	}
+	
+	$routes[$f]= $route;
+
 }
-echo json_encode($route);
+echo json_encode($routes);
 ?>
