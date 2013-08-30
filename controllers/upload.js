e6d36cd5168229b08bf20ab6932c39e9413438af
neoadventist <git@spencersims.com>
2013-08-29 17:04:28 -0700

    On upload, each individual route gets its own map

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,21 +1,27 @@
 app.controller('upload', function ($scope, $timeout, $filter,sharedData) {
 	$scope.header="Upload Your Ride!";
 	$scope.name = sharedData.getName().name;
+	$scope.mapsId=[];
 	$scope.uploadComplete = function (content, completed) {
 		if (completed && content.length > 0) {
 			$scope.response = (content); // Presumed content is a json string!
 			console.log($scope.response);
 			sharedData.saveRoutes($scope.response);
 			for($currentRoute=0;$currentRoute<$scope.response.length;$currentRoute++){
-				drawRoute($scope.response[$currentRoute]);
+				m = initMap($currentRoute);
+				drawRoute($scope.response[$currentRoute],m);
 			}
 			
 		}
 	};
 	
-initMap = function(){
+	
+	
+var initMap = function(id){
 	// set up the map
-	$scope.map = new L.Map('map');
+	$scope.mapsId.push("route"+id);
+	$scope.$apply();
+	map = new L.Map($scope.mapsId[id]);
 
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -23,12 +29,13 @@ initMap = function(){
 	var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 20, attribution: osmAttrib});		
 
 	// start the map at Cheif Lunes Meet Spot
-	$scope.map.setView(new L.LatLng(34.033235,-118.2835057),10);
-	$scope.map.addLayer(osm);
+	map.setView(new L.LatLng(34.033235,-118.2835057),10);
+	map.addLayer(osm);
+	return map;
 };
-initMap();
+//initMap(0);
 	
-drawRoute = function(route){
+var drawRoute = function(route,map){
 	var data=[]; 
 	for (i=1;i<route.length-1;i++){
 		data.push([route[i][0], route[i][1]]);
@@ -39,9 +46,9 @@ drawRoute = function(route){
 		"weight": 7,
 		"opacity": 0.40
 	};
-	var track = L.polyline(data, polyline_options).addTo($scope.map);
+	var track = L.polyline(data, polyline_options).addTo(map);
 	// zoom the map to the route
-	//window.map.fitBounds(track.getBounds());
+	map.fitBounds(track.getBounds());
 };
 	
 });
